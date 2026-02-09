5973f933a593655049e7af4b479105e001783082
chbsaikiran <sai.406@gmail.com>
2026-02-09 14:08:53 +0530

    added code save the trained model

--- Pytorch/working_with_data.py
+++ Pytorch/working_with_data.py
@@ -85,6 +85,9 @@ for t in range(epochs):
 	test(test_dataloader,model,loss_fn)
 
 print("Done!!")
+
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
 			
 		
 	
